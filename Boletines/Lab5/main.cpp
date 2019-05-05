141f6201ba9695cdb1f45b9586e5e3e7224616af
floghos <caleuche94@gmail.com>
2019-05-04 20:10:35 -0400

    added code to test times in main.cpp

--- Boletines/Lab5/main.cpp
+++ Boletines/Lab5/main.cpp
@@ -5,34 +5,81 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
     myQueue q;
-    int n = 20;
+    int n;
 
-    cout << q.empty() << "\n\n";
+	cin >> n;
+	cout << "n = " << n << '\n';
+	
+	clock_t t1 = clock();
     for (int i = 0; i < n; i++) { //llenar la queue
         q.push(i);
     }
-    cout << "size? " << q.size() << '\n';
-	cout << "empty? " << q.empty() << "\n\n";
+	clock_t t2 = clock();
+	double tiempo = (double)(t2-t1)/CLOCKS_PER_SEC;
+	printf("push(): %.10f\n", tiempo);
+	///////////////////////////////////////////////////////////////////////
+
+	t1 = clock();
+    for (int i = 0; i < 100; i++) {
+        q.front();
+    }
+	t2 = clock();
+	tiempo = (double)(t2-t1)/CLOCKS_PER_SEC;
+	tiempo /= 100;
+	printf("front(): %.10f\n", tiempo);
+
+	///////////////////////////////////////////////////////////////////////
+
+	t1 = clock();
+    for (int i = 0; i < 100; i++) {
+        q.back();
+    }
+	t2 = clock();
+	tiempo = (double)(t2-t1)/CLOCKS_PER_SEC;
+	tiempo /= 100;
+	printf("back(): %.10f\n", tiempo);
+
+	///////////////////////////////////////////////////////////////////////
+
+	t1 = clock();
+    for (int i = 0; i < 100; i++) {
+        q.size();
+    }
+	t2 = clock();
+	tiempo = (double)(t2-t1)/CLOCKS_PER_SEC;
+	tiempo /= 100;
+	printf("size(): %.10f\n", tiempo);
+
+	///////////////////////////////////////////////////////////////////////
+
+	t1 = clock();
+    for (int i = 0; i < n; i++) { //llenar la queue
+        q.push(i);
+    }
+	t2 = clock();
+	tiempo = (double)(t2-t1)/CLOCKS_PER_SEC;
+	printf("pop(): %.10f\n", tiempo);
+
+	///////////////////////////////////////////////////////////////////////
+
+	// void push(int data);
+	// int front();
+	// int back();
+	// int size();
+	// void pop();
 
 
-	qIterator *it = q.elements();
-	for (int i = 0; i < n; i++) {
-		cout << "tiene siguiente? " << it->hasNext() << '\n';
-		if (it->hasNext()) {
-			cout << it->next() << '\n';
-		}
-	}
+	// qIterator *it = q.elements();
+	// for (int i = 0; i < n; i++) {
+	// 	cout << "tiene siguiente? " << it->hasNext() << '\n';
+	// 	if (it->hasNext()) {
+	// 		cout << it->next() << '\n';
+	// 	}
+	// 	if (i == 6) {
+	// 		it->reset();
+	// 	}
+	// }
 
 
     return 0;
 }
-
-
-// for (int i = 0; i < n; i++) { //vaciar la queue
-	//     cout << q.front();
-	//     q.pop();
-	//     cout << "(" << q.size() << ") ";
-	// }
-	// cout << '\n';
-	// cout << q.size() << '\n';
-	// cout << q.empty() << '\n';
