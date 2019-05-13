8b49dac2c04f20c083eb109d8488f317978616a5
floghos <caleuche94@gmail.com>
2019-05-13 17:12:02 -0400

    finished implementing all priority queues. Need to test them all!!

--- Boletines/Lab6/PriorityQueueHeap.h
+++ Boletines/Lab6/PriorityQueueHeap.h
@@ -1,12 +1,21 @@
 #include "ADTPriorityQueue.h"
+#include <bits/stdc++.h>
+
 
 class PriorityQueueHeap: public ADTPriorityQueue {
 private:
-    vector<int> heap;
+    const int head = 1;
+    std::vector<int> heap;
+    int _size;
+
+    void upheap(int node);
+    void downheap(int node);
+    
 public:
+    PriorityQueueHeap();
     int top();
     void pop();
-    void push(int data);
+    void push(int newData);
     int size();
     bool empty();
 };
--- Boletines/Lab6/main.cpp
+++ Boletines/Lab6/main.cpp
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
 #include "ADTPriorityQueue.h"
+#include "PriorityQueueHeap.h"
+#include "PriorityQueueSorted.h"
+#include "PriorityQueueUnsorted.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
+    PriorityQueueHeap heapQ;
+    PriorityQueueSorted sortedQ;
+    PriorityQueueUnsorted unsortedQ;
 
     return 0;
 }
