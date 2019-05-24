714cc823fd8e4a1899c4a317487da1ba9fdf8830
floghos <caleuche94@gmail.com>
2019-05-24 17:25:29 -0400

    añadimos mensajes para saber mas de los resultados

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -6,10 +6,13 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-	// KdTree kT;
-	QuadTree qT;
+	KdTree kT;
+	// QuadTree qT;
+
 	vector<pair<int, int> > puntos;
+
 	int n, m, p;
+	int out_of_bounds = 0;
 	cin >> n >> m >> p;
 
 	for (int i = 0; i < p; i++) {
@@ -17,7 +20,7 @@ int main(int argc, char const *argv[]) {
 		int f, c;
 		cin >> f >> c;
 		if(f < 0 || c < 0 || f > n-1 || c > m-1) { //verificamos que los puntos esten dentro del rango
-
+			out_of_bounds++;
 		} else {
 			pto.first = f;
 			pto.second = c;
@@ -26,18 +29,22 @@ int main(int argc, char const *argv[]) {
 
 	}
 
-	qT.construir(puntos);
+	kT.construir(puntos);
+	// qT.construir(puntos);
 
 	int x1, y1, x2, y2;
 	cin >> x1 >> y1 >> x2 >> y2;
 
-	vector<pair<int, int> > puntosEncontrados = qT.buscar(x1,y1, x2, y2);
+	vector<pair<int, int> > puntosEncontrados = kT.buscar(x1,y1, x2, y2);
+	// vector<pair<int, int> > puntosEncontrados = qT.buscar(x1,y1, x2, y2);
+	cout << "Puntos fuera de rango: " << out_of_bounds << '\n';
+	cout << "El total de puntos en el arbol es: " << puntos.size() << '\n';
+	cout << "Los puntos encontrados dentro de el rango dado son: " << '\n';
 
-	cout << "\n\nLos puntos encontrados dentro de el rago dado son: " << '\n';
 	for (int i = 0; i < puntosEncontrados.size(); i++) {
-		cout << "(" << puntosEncontrados[i].first << ", " << puntosEncontrados[i].second << "); ";
+		cout << "(" << puntosEncontrados[i].first << ", " << puntosEncontrados[i].second << ")\n";
 	}
-	cout << '\n';
+	// cout << '\n';
 
 
 	return 0;
