cf594a8bc7630d9db9372fae8c27e3097c0ac3a8
floghos <caleuche94@gmail.com>
2019-05-24 17:30:11 -0400

    programa para adaptar los conjuntos impresos por python

---
+++ proyecto/generador/adaptador.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	int n, x, y;
+	char a, b, c;
+	scanf("%d\n", &n);
+	cout << n << '\n';
+	for (int i = 0; i < n; i++) {
+		scanf("%c%d%c %d%c\n", &a, &x, &b, &y, &c);
+		cout << x << " " << y << '\n';
+	}
+
+	return 0;
+}
