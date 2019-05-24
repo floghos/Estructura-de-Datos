7e5597a6bb9ec4c8a5220c9b05c0114bef48fc2c
floghos <caleuche94@gmail.com>
2019-05-24 13:18:53 -0400

    añadimos restricciones para verificar que los puntos esten en el rango
    Co-Authored-By: Javiera <javi.loxar@gmail.com>

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -16,24 +16,29 @@ int main(int argc, char const *argv[]) {
 		pair<int, int> pto;
 		int f, c;
 		cin >> f >> c;
-		pto.first = f;
-		pto.second = c;
-		puntos.push_back(pto);
+		if(f < 0 || c < 0 || f > n-1 || c > m-1) { //verificamos que los puntos esten dentro del rango
+
+		} else {
+			pto.first = f;
+			pto.second = c;
+			puntos.push_back(pto);
+		}
+
 	}
 
 	qT.construir(puntos);
 
-	// int x1, y1, x2, y2;
-	// cin >> x1 >> y1 >> x2 >> y2;
-	//
-	// vector<pair<int, int> > puntosEncontrados = kT.buscar(x1,y1, x2, y2);
-	//
-	// cout << "\n\nLos puntos encontrados dentro de el rago dado son: " << '\n';
-	// for (int i = 0; i < puntosEncontrados.size(); i++) {
-	// 	cout << "(" << puntosEncontrados[i].first << ", " << puntosEncontrados[i].second << "); ";
-	// }
-	// cout << '\n';
-	//
+	int x1, y1, x2, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+
+	vector<pair<int, int> > puntosEncontrados = qT.buscar(x1,y1, x2, y2);
+
+	cout << "\n\nLos puntos encontrados dentro de el rago dado son: " << '\n';
+	for (int i = 0; i < puntosEncontrados.size(); i++) {
+		cout << "(" << puntosEncontrados[i].first << ", " << puntosEncontrados[i].second << "); ";
+	}
+	cout << '\n';
+
 
 	return 0;
 }
