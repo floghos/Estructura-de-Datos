193870fff1a6bc5c111687972a51edd4c7a3f694
floghos <caleuche94@gmail.com>
2019-05-26 15:55:08 -0400

    Inicializacion de Xs e Ys en main

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -36,7 +36,11 @@ int main(int argc, char const *argv[]) {
 	// qT.construir(puntos);
 	cout << "Arbol creado\n";
 
-	int x1, y1, x2, y2;
+	int x1 = 0,
+		y1 = 0,
+		x2 = 0,
+		y2 = 0;
+		
 	cin >> x1 >> y1 >> x2 >> y2;
 
 	int cant_puntos_dentro_del_rango = checkeoFuerzaBruta(puntos, x1, y1, x2, y2);
