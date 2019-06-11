0d7628aaabcb2d405d8445f6d9afcf9357d166e2
floghos <caleuche94@gmail.com>
2019-06-11 14:40:48 -0400

    cambio de Map basado en hash por la implementacion de la Javi

--- proyecto2/MapH.cpp
+++ proyecto2/MapH.cpp
@@ -1,127 +1,114 @@
-#include "MapH.h"
 #include <string>
 #include <iostream>
+#include "MapH.h"
 
 using namespace std;
 
-//Private Methods
-int MapH::goodhashCode(string key) {
-	int hash = 13;
-	for (int i = 0; i < key.length(); i++) {
-		hash += key.at(i) * (1 << i);
+int MapH::primos[] = {53,97,193,389,769,1543,3079,6151,12289,24593,49157,98317,196613,393241,786433,1572869,3145739,6291469,12582917,25165843,50331653,100663319,201326611,402653189,805306457,1610612741};
+
+MapH::MapH(){
+	indice = 0;
+	capacidad = primos[0];
+	array = new pair<string, int> [capacidad];
+	tam = 0;
+	for(int i=0; i<capacidad; i++){
+		array[i].first = "\n";
 	}
-	return hash;
 }
 
-int MapH::secondHash(string key) {
-	int prime = primeSizes[pIndex - 2];
-	int hash = prime - (goodhashCode(key) % prime);
-	return hash;
+void MapH::insert(pair<string, int> par){
+	if(tam == capacidad) rehashing();
+	int pos = fPol1(par.first);
+	int desp = fPol2(par.first);
+	for(int i=0; i<capacidad; i++){
+		if(array[pos].first == "\n" || array[pos].first == "1"){
+			array[pos].second = par.second;
+			array[pos].first = par.first;
+			tam++;
+			break;
+		}else if(array[pos].first == par.first){
+			cout<<"Ya hay un elemento con la clave ingresada, no se puede añadir: ("<<par.second<<", "<<par.first<<")"<<endl;
+			break;
+		}
+		pos = (pos+desp)%capacidad;
+	}
 }
 
-void MapH::rehash() {
-	pIndex++;
-	int newCapacity = primeSizes[pIndex];
-	bucket *newMap = new bucket[newCapacity];
-	for (int i = 0; i < capacity; i++) {
-		if (hashTable[i].available) {
-			continue;
-		} else {
-			int pos = goodhashCode(hashTable[i].key) % newCapacity;
-			while (!newMap[pos].available) {
-				pos += secondHash(hashTable[i].key); //double hashing
-				pos %= newCapacity;
+void MapH::erase(string clave){
+	int pos = fPol1(clave);
+	int desp = fPol2(clave);
+	for(int i=0; i<capacidad; i++){
+		if(array[pos].first == clave){
+			array[pos].second = 0;
+			array[pos].first = "1";
+			tam--;
+			break;
+		}else if(array[pos].first == "\n") break;
+		pos = (pos+desp)%capacidad;
+	}
+}
+
+int MapH::at(string clave){
+	int pos = fPol1(clave);
+	int desp = fPol2(clave);
+	for(int i=0; i<capacidad; i++){
+		if(array[pos].first == clave){
+			return array[pos].second;
+		}else if(array[pos].first == "\n"){
+			return -1;
+		}
+		pos = (pos+desp)%capacidad;
+	}
+	return -1;
+}
+
+int MapH::size(){
+	return tam;
+}
+
+bool MapH::empty(){
+	return !tam;
+}
+
+int MapH::fPol1(string clave){
+	int pos = 0;
+	int z = 33;
+	for(int i=0; i<clave.size(); i++){
+		pos = (z*pos)%capacidad + clave[i];
+	}
+	pos = pos%capacidad;
+	return pos;
+}
+
+int MapH::fPol2(string clave){
+	int pos = 0;
+	int z = 7;
+	for(int i=0; i<clave.size(); i++){
+		pos = (z*pos)%capacidad + clave[i];
+	}
+	pos = (pos%(capacidad-2))+1;
+	return pos;
+}
+
+void MapH::rehashing(){
+	indice++;
+	capacidad = primos[indice];
+	pair<string, int>* aux = new pair<string, int> [capacidad];
+	for(int i=0; i<capacidad; i++){
+		aux[i].first = "\n";
+	}
+	int pos, desp;
+	for(int i=0; i<tam; i++){
+		pos = fPol1(array[i].first);
+		desp = fPol2(array[i].first);
+		for(int j=0; j<capacidad; j++){
+			if(aux[pos].first == "\n"){
+				aux[pos] = array[i];
+				break;
 			}
-			newMap[pos].key = hashTable[i].key;
-			newMap[pos].val = hashTable[i].val;
-			newMap[pos].available = false;
+			pos = (pos+desp)%capacidad;
 		}
 	}
-	delete[] hashTable;
-	hashTable = newMap;
-	capacity = newCapacity;
-}
-
-bool MapH::isFull() {
-	return _size == capacity;
-}
-
-float MapH::loadFactor() { //I may not even use this
-	return (float)_size/capacity;
-}
-
-//Public Methods
-MapH::MapH() {
-	_size = 0;
-	pIndex = 2;
-    capacity = primeSizes[pIndex];
-    hashTable = new bucket[capacity];
-}
-
-void MapH::insert(pair<string, int> data) {
-	int pos = goodhashCode(data.first) % capacity;
-	while (!hashTable[pos].available) {
-		pos += secondHash(data.first); //double hashing
-		pos %= capacity;
-	}
-
-	hashTable[pos].key = data.first;
-	hashTable[pos].val = data.second;
-	hashTable[pos].available = false;
-	_size++;
-	if (isFull()) {
-		rehash();
-	}
-}
-
-void MapH::erase(string key) {
-	bool error_404 = false; //key not found
-
-	int pos = goodhashCode(key) % capacity;
-	int startingPos = pos;
-	while (hashTable[pos].key.compare(key) != 0) { //linear probing
-		if (hashTable[pos].key.empty() || pos == startingPos) {
-			error_404 = true;
-			break;
-		}
-		pos += secondHash(key); //double hashing
-		pos %= capacity;
-	}
-
-	if (error_404) {
-		// cout << "Error 404: String not found\n";
-	} else {
-		hashTable[pos].available = true;
-		_size--;
-	}
-}
-
-int MapH::at(string key) {
-	bool error_404 = false; //key not found
-
-	int pos = goodhashCode(key) % capacity;
-	int startingPos = pos;
-	while (hashTable[pos].available || hashTable[pos].key.compare(key) != 0) { //linear probing
-		if (hashTable[pos].key.empty() || pos == startingPos) {
-			error_404 = true;
-			break;
-		}
-		pos += secondHash(key); //double hashing
-		pos %= capacity;
-	}
-
-	if (error_404) {
-		// cout << "Error 404: String not found\n";
-		return -1;
-	} else {
-		return hashTable[pos].val;
-	}
-}
-
-int MapH::size() {
-    return _size;
-}
-
-bool MapH::empty() {
-    return !_size;
+	delete[] array;
+	array = aux;
 }
--- proyecto2/MapH.h
+++ proyecto2/MapH.h
@@ -1,32 +1,22 @@
 #include "ADTMap.h"
+#include <string>
+using namespace std;
 
-struct bucket {
-	int val;
-	// string key = string();
-	string key;
-	bool available = true;
-};
-
-static int primeSizes[] = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289,
-	24593, 49157, 98317, 196613, 393241, 786433, 1572869, 3145739, 6291469, 12582917};
-
-
-class MapH: public ADTMap {
-private:
-	int pIndex;
-	bucket *hashTable;
-	int _size;
-	int capacity;
-	int goodhashCode(string key);
-	int secondHash(string key);
-    void rehash();
-    bool isFull();
-	float loadFactor();
-public:
-	MapH();
-	void insert(pair<string, int> entry);
-	int at(string key);
-	void erase(string key);
-	bool empty();
-	int size();
+class MapH : public ADTMap {
+	private:
+		static int primos[];
+		pair<string, int>* array;
+		int tam;
+		int capacidad;
+		int indice;
+		void rehashing();
+		int fPol1(string s);
+		int fPol2(string s);
+	public:
+		MapH();
+		void insert(pair<string, int>);
+		void erase(string);
+		int at(string);
+		int size();
+		bool empty();
 };
